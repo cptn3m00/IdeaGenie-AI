291491b465f4271ca221cd24c9c268db4323b938
Akash <akashtjsamajdar@gmail.com>
2024-09-03 12:45:23 +0530

    template update

--- app/(data)/Template.tsx
+++ app/(data)/Template.tsx
@@ -249,5 +249,77 @@ export default [
                 required: true
             }
         ]
-    }
+    },
+    {
+        name: 'English Grammer Check',
+        desc: 'AI Model to Correct your english grammer by providing the text',
+        icon:'https://cdn-icons-png.flaticon.com/128/12596/12596700.png',
+        category: 'english',
+       
+        slug: 'english-grammer-checker',
+        aiPrompt: 'Rewrite the inputText by correcting the grammer and give output in  in rich text editor format',
+        form: [
+            {
+                label: 'Enter text to correct the grammer',
+                field: 'input',
+                name: 'inputText',
+                required:true
+            },
+           
+        ]
+    },
+    {
+        name: 'Write Code',
+        desc: 'AI Model to generate programming code in any language',
+        icon:'https://cdn-icons-png.flaticon.com/128/6062/6062646.png',
+        category: 'Coding',
+       
+        slug: 'write-code',
+        aiPrompt: 'Depends on user codeDescription write a code and give output in  in rich text editor format in code block ',
+        form: [
+            {
+                label: 'Enter description of code you want along with Programming Lang',
+                field: 'textarea',
+                name: 'codeDesscripton',
+                required:true
+            },
+           
+        ]
+    },
+    {
+        name: 'Explain Code',
+        desc: 'AI Model to explain programming code in any language',
+        icon:'https://cdn-icons-png.flaticon.com/128/8488/8488751.png',
+        category: 'Coding',
+       
+        slug: 'explain-code',
+        aiPrompt: 'Depends on user codeDescription explain code line by line and give output in  in rich text editor format in code block ',
+        form: [
+            {
+                label: 'Enter code which you want to understand',
+                field: 'textarea',
+                name: 'codeDesscripton',
+                required:true
+            },
+           
+        ]
+    },
+    {
+        name: 'Code Bug Detector',
+        desc: 'This tool analyzes your input, like error messages and code snippets, to pinpoint and fix bugs, offering detailed solutions and alternatives in a straightforward, user-friendly way.',
+        icon:'https://cdn-icons-png.flaticon.com/128/4426/4426267.png',
+        category: 'code-bug-detector',
+       
+        slug: 'code-bug-detector',
+        aiPrompt: 'Depends on user codeInput find bug in code and give solution and give output in  in rich text editor format in code block ',
+        form: [
+            {
+                label: 'Enter code which you want to test bug',
+                field: 'textarea',
+                name: 'codeInput',
+                required:true
+            },
+           
+        ]
+    },
 ];
